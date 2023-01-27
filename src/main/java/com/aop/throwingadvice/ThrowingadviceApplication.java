2eb9d0587abda3a9e165dcfcee102c36c721addf
Martin Dymenstein <martin.dymenstein@thomsonreuters.com>
2023-01-27 16:14:12 -0300

    init

---
+++ src/main/java/com/aop/throwingadvice/ThrowingadviceApplication.java
@@ -0,0 +1,13 @@
+package com.aop.throwingadvice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThrowingadviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThrowingadviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aop/throwingadvice/ThrowingadviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aop.throwingadvice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThrowingadviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
